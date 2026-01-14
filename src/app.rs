0b45e95b3749a29e35c16215c837098e99ff4576
Cameron Howell <jango.djarin@proton.me>
2026-01-13 20:34:30 -0600

    Formatting and some UI cleanup

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,8 @@
+use regex::Regex;
 use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
 use wasm_bindgen_futures::spawn_local;
+use web_sys::console::debug;
 use yew::prelude::*;
 use gloo_net::http::Request;
 use std::fmt;
@@ -93,6 +95,34 @@ async fn fetch_risk(map_server: MapServer) -> Result<i32, String> {
     }
 }
 
+fn parse_temps(lines: &[&str]) -> Option<(i32, i32)> {
+    let mut high = None;
+    let mut low = None;
+
+    for line in lines {
+        let parts: Vec<&str> = line.trim().split_whitespace().collect();
+        web_sys::console::log_1(&JsValue::from_str(&format!("{:?}", parts).to_string()));
+        if parts.first() == Some(&"TXN") {
+            low = parts.get(1)?.parse::<i32>().ok();
+            high = parts.get(2)?.parse::<i32>().ok();
+        }
+    }
+
+    match (high, low) {
+        (Some(h), Some(l)) => Some((h, l)),
+        _ => None,
+    }
+}
+
+async fn fetch_daycast() -> Result<(i32, i32), String> {
+    let url = "https://blend.mdl.nws.noaa.gov/nbm-text-new?ele=NBS&sta=KHSV&cyc=Latest";
+    let response = Request::get(&url).send().await.map_err(|e| e.to_string())?.text().await.map_err(|e| e.to_string())?;
+    let lines: Vec<_> = response.lines().collect();
+    let (hi, lo) = parse_temps(&lines).ok_or_else(|| "Temps not found")?;
+
+    Ok((hi, lo))
+}
+
 #[component]
 fn Outlook() -> Html {
     let state = use_context::<UseStateHandle<AppState>>().expect("AppState context not found");
@@ -158,6 +188,51 @@ fn GetRisk(MapServerProps { map_server }: &MapServerProps) -> Html {
     }
 }
 
+#[component]
+fn GetTemp() -> Html {
+    let temps = use_state(|| (None::<i32>, None::<i32>));
+
+    {
+        let temps = temps.clone();
+        use_effect_with((), move |_| {
+            wasm_bindgen_futures::spawn_local(async move {
+                let result = fetch_daycast().await;
+                if let Ok(r) = result {
+                    temps.set((Some(r.0), Some(r.1)));
+                } else {
+                    web_sys::console::log_1(&JsValue::from_str(&format!("{:?}", result).to_string()));
+                }
+            });
+            || ()
+        });
+    }
+
+    html! {
+        <div>
+            {
+                match &*temps {
+                    (Some(h), Some(l)) => {
+                        html! {
+                            <>
+                                <p style="font-size: 16pt;">{format!("{h}°F")}</p>
+                                <p>{format!("{l}°F")}</p>
+                            </>
+                        }
+                    }
+                    _ => {
+                        html! {
+                            <>
+                                <p style="font-size: 16pt;">{"-"}</p>
+                                <p>{"-"}</p>
+                            </>
+                        }
+                    }
+                }
+            }
+        </div>
+    }
+}
+
 #[derive(Clone, PartialEq)]
 pub struct AppState {
     pub outlook_url: String,
@@ -186,10 +261,10 @@ pub fn OutlookButtons() -> Html {
 
     html! {
         <>
-            <button style="margin-right: 16px; width: 150px;" onmouseenter={change_outlook("https://www.spc.noaa.gov/products/outlook/day1otlk.gif")}>{"Categorical"}</button>
-            <button style="margin-right: 16px; width: 150px;" onmouseenter={change_outlook("https://www.spc.noaa.gov/products/outlook/day1probotlk_torn.gif")}>{"Tornado"}</button>
-            <button style="margin-right: 16px; width: 150px;" onmouseenter={change_outlook("https://www.spc.noaa.gov/products/outlook/day1probotlk_wind.gif")}>{"Wind"}</button>
-            <button style="width: 150px;" onmouseenter={change_outlook("https://www.spc.noaa.gov/products/outlook/day1probotlk_hail.gif")}>{"Hail"}</button>
+            <button style="margin-right: 16px; width: 100px;" onmouseenter={change_outlook("https://www.spc.noaa.gov/products/outlook/day1otlk.gif")}>{"Categorical"}</button>
+            <button style="margin-right: 16px; width: 100px;" onmouseenter={change_outlook("https://www.spc.noaa.gov/products/outlook/day1probotlk_torn.gif")}>{"Tornado"}</button>
+            <button style="margin-right: 16px; width: 100px;" onmouseenter={change_outlook("https://www.spc.noaa.gov/products/outlook/day1probotlk_wind.gif")}>{"Wind"}</button>
+            <button style="width: 100px;" onmouseenter={change_outlook("https://www.spc.noaa.gov/products/outlook/day1probotlk_hail.gif")}>{"Hail"}</button>
         </>
     }
 }
@@ -205,29 +280,35 @@ pub fn App() -> Html {
                     <strong>{"Meso"}</strong>
                     {" | Weather Dashboard"}
                 </header>
-                <main class="container">
-                    <section class="panel">
-                        <h2>{"Day 1 Categorical Outlook"}</h2>
-                        <GetRisk map_server={MapServer::Day1Outlook} />
-                        <h2>{"Risks by Type"}</h2>
-                        <div class="status-grid">
-                            <div style="display: flex; flex-direction: row;">
-                                <div class="status-item" style="width: 150px;">
-                                    <span class="label">{"Tornado"}</span>
-                                    <span class="value"><GetRisk map_server={MapServer::Day1Tornado} /></span>
-                                </div>
-                                <div class="status-item" style="width: 150px;">
-                                    <span class="label">{"Wind"}</span>
-                                    <span class="value"><GetRisk map_server={MapServer::Day1Wind} /></span>
-                                </div>
-                                <div class="status-item" style="width: 150px;">
-                                    <span class="label">{"Hail"}</span>
-                                    <span class="value"><GetRisk map_server={MapServer::Day1Hail} /></span>
+                <main class="container" style="align-items: center;">
+                    <div class="status-row">
+                        <section class="panel">
+                            <h2>{"Daycast"}</h2>
+                            <GetTemp />
+                        </section>
+                        <section class="panel">
+                            <h2>{"Day 1 Categorical Outlook"}</h2>
+                            <GetRisk map_server={MapServer::Day1Outlook} />
+                            <h2>{"Risks by Type"}</h2>
+                            <div class="status-grid">
+                                <div class="status-row">
+                                    <div class="status-item" style="width: 150px;">
+                                        <span class="label">{"Tornado"}</span>
+                                        <span class="value"><GetRisk map_server={MapServer::Day1Tornado} /></span>
+                                    </div>
+                                    <div class="status-item" style="width: 150px;">
+                                        <span class="label">{"Wind"}</span>
+                                        <span class="value"><GetRisk map_server={MapServer::Day1Wind} /></span>
+                                    </div>
+                                    <div class="status-item" style="width: 150px;">
+                                        <span class="label">{"Hail"}</span>
+                                        <span class="value"><GetRisk map_server={MapServer::Day1Hail} /></span>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </section>
-                    <section class="panel">
+                        </section>
+                    </div>
+                    <section class="panel" style="width: 675px;">
                         <h2>{"SPC Outlook Map"}</h2>
                         <div class="status-item">
                             <OutlookButtons />
@@ -236,7 +317,9 @@ pub fn App() -> Html {
                             <Outlook />
                         </div>
                         <h2>{"6-10 Day Climate Outlook"}</h2>
-                        <Climate />
+                        <div class="status-item">
+                            <Climate />
+                        </div>
                     </section>
                 </main>
                 <footer class="attribution">
